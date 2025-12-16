3ec9d25f6010786541d957e448ed2ee88a05556a
ShimaMomen <shimasadat.momen@outlook.de>
2025-12-16 13:10:38 +0100

    login page tests

--- Automatisierten Testens/tests/e2e/login.spec.ts
+++ Automatisierten Testens/tests/e2e/login.spec.ts
@@ -1,32 +1,40 @@
 import { test, expect } from '@playwright/test';
-import { LoginPage } from '../../pages/LoginPage';
 
 
-test.describe('Login tests', () => {
 
-  test('successful login', async ({ page }) => {
-    const loginPage = new LoginPage(page);
 
-    await loginPage.goto();
-    await loginPage.login('standard_user', 'secret_sauce');
+test ('Login with valid credentials', async ({ page }) => {
 
-    await expect(page).toHaveURL(/inventory/);
-  });
+await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+await page.getByRole('button', { name: 'Login' }).click();
 
-  test('invalid login shows error', async ({ page }) => {
-    const loginPage = new LoginPage(page);
-
-    await loginPage.goto();
-    await loginPage.login('wrong', 'wrong');
-
-    await expect(await loginPage.getErrorMessage())
-      .toContain('Username and password do not match');
-  });
+await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
 
 });
 
+test ('Login with invalid credentials', async ({ page }) => {
 
-  
+await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+await page.getByRole('textbox', { name: 'Username' }).fill('Admin1');
+await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+await page.getByRole('button', { name: 'Login' }).click();
+
+const errorMessage = page.getByRole('alert');
+await expect(errorMessage).toHaveText('Invalid credentials'); 
 
 
+    });
 
+test ('Login with missing password', async ({ page }) => {
+
+await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+await page.getByRole('textbox', { name: 'Password' }).fill(' ');
+await page.getByRole('button', { name: 'Login' }).click();
+const requiredfield = page.locator('.oxd-input-field-error-message');
+await expect(requiredfield).toHaveText('Required');
+
+
+   });
